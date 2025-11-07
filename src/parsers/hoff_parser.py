3790f552dbb689e07b23733b814f26ae61dcd220
MikhailBasiy <mikhail.basiy@gmail.com>
2025-11-07 15:53:57 +0300

    feat: update hoff parser

--- src/parsers/hoff_parser.py
+++ src/parsers/hoff_parser.py
@@ -1,6 +1,6 @@
 import undetected_chromedriver as uc
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
