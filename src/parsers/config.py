f5d2057ba6bea839e075ed4945162837c7dbf9ef
MikhailBasiy <mikhail.basiy@gmail.com>
2025-11-07 15:55:19 +0300

    feat: add Novosibirsk coordinates in config file

--- src/parsers/config.py
+++ src/parsers/config.py
@@ -1,3 +1,4 @@
 geo_settings = {
-    "Москва": {"latitude": 55.748199, "longitude": 37.585224, "accuracy": 100}
+    "Москва": {"latitude": 55.748199, "longitude": 37.585224, "accuracy": 100},
+    "Новосибирск": {"latitude": 55.045470, "longitude": 82.917869, "accuracy": 100},
 }
