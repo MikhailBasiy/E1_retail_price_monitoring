aed0fd7c631e6fa09bf5ed39efb57885f7863a44
MikhailBasiy <mikhail.basiy@gmail.com>
2026-07-15 17:23:06 +0300

    feat: update geolocation coordinates

--- src/parsers/config.py
+++ src/parsers/config.py
@@ -1,4 +1,8 @@
 geo_settings = {
-    "Москва": {"latitude": 55.748199, "longitude": 37.585224, "accuracy": 100},
+    "Москва": {
+        "latitude": 55.76061940970195,
+        "longitude": 37.61205517887951,
+        "accuracy": 100,
+    },
     "Новосибирск": {"latitude": 55.045470, "longitude": 82.917869, "accuracy": 100},
 }
